49487f2fd61a653130aa2f0a6fe8acf04321a6b2
Marvin <1254401125@mil>
2019-10-11 12:39:28 -0500

    Added file reading

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-console.log("successful import");
 window.onload = function() {
+    const fs = require('fs');
+
+    fs.readFile('testNames.txt', (err, data) => {
+        if (err) throw err;
+
+        console.log(data.toString());
+    });
+
     var ctx = document.getElementById('myChart').getContext('2d');
     var chart = new Chart(ctx, {
         // The type of chart we want to create
