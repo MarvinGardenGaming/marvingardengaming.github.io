4336691e7972d444b44f475abc41aa3b06dc30d5
Zachary Marvin <zachary.marvin@live.com>
2019-10-11 18:59:34 -0500

    Minor Changes

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-window.onload = function() {
+var nameArray = [];
+var genderArray = [];
+var countArray = [];
+var yearArray = [];
 
-    var nameArray = [];
-    var genderArray = [];
-    var countArray = [];
-    var yearArray = [];
+window.onload = function() {
 
     $.get("testNames.txt", function(data){
         var rows = data.split("\n");
@@ -55,4 +55,44 @@ function makeSearch(){
     console.log("function began");
     nameToSearch = document.getElementById("enterName").value;
     console.log(nameToSearch);
+    $.get("masterList.txt", function(data){
+        var rows = data.split("\n");
+        console.log("rows are: ");
+        console.log(rows);
+        for(row in rows){
+            var columnsInRow = rows[row].split(",");
+            var name = columnsInRow[0];
+            var gender = columnsInRow[1];
+            var count = columnsInRow[2];
+            var year = columnsInRow[3];
+
+            if(name == nameToSearch){
+                console.log("match found")
+                nameArray.push(name);
+                genderArray.push(gender);
+                countArray.push(count);
+                yearArray.push(year);
+            }
+        }
+
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
+    
+            // The data for our dataset
+            data: {
+                labels: nameArray,
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: countArray
+                }]
+            },
+    
+            // Configuration options go here
+            options: {}
+        });
+    });
 };
