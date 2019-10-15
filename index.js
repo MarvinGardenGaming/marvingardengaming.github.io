b30fff5cc582131f562e3d2a86968c4ec3127acc
Marvin <1254401125@mil>
2019-10-15 14:17:16 -0500

    Testing Vertical tabs

--- index.js
+++ index.js
@@ -9,8 +9,6 @@ window.onload = function() {
 
     $.get("testNames.txt", function(data){
         var rows = data.split("\n");
-        console.log("rows are: ");
-        console.log(rows);
         for(row in rows){
             var columnsInRow = rows[row].split(",");
             var name = columnsInRow[0];
@@ -23,14 +21,6 @@ window.onload = function() {
             countArray.push(count);
             yearArray.push(year);
         }
-
-        console.log(data.toString());
-
-        console.log("Names are");
-        console.log(nameArray);
-        console.log("Counts are");
-        console.log(countArray);
-    
         ctx = document.getElementById('myChart').getContext('2d');
         chart = new Chart(ctx, {
             // The type of chart we want to create
@@ -54,7 +44,6 @@ window.onload = function() {
 };
 
 function makeSearch(){
-    console.log("starting make search function and clearing data arrays");
     nameArray = [];
     genderArray = [];
     countArray = [];
@@ -70,39 +59,15 @@ function makeSearch(){
             var year = columnsInRow[3];
 
             if(name == nameToSearch && year == "2000"){
-                console.log("match found")
                 nameArray.push(name);
                 genderArray.push(gender);
                 countArray.push(count);
                 yearArray.push(year);
             }
         };
-        if(chart){
-            console.log("chart exists");
-            chart.config.data.labels = nameArray;
-            chart.config.data.datasets.data = countArray;
-            chart.update();
-        } else {
-            console.log("chart doesnt exist");
-            ctx = document.getElementById('myChart').getContext('2d');
-            chart = new Chart(ctx, {
-                // The type of chart we want to create
-                type: 'bar',
-    
-                // The data for our dataset
-                data: {
-                    labels: yearArray,
-                    datasets: [{
-                        label: 'My First dataset',
-                        backgroundColor: 'rgb(255, 99, 132)',
-                        borderColor: 'rgb(255, 99, 132)',
-                        data: countArray
-                    }]
-                },
-    
-                // Configuration options go here
-                options: {}
-            });
-        };
+
+        chart.config.data.labels = yearArray;
+        chart.config.data.datasets.data = countArray;
+        chart.update();
     });
 };
