78551e9b7ffc33bef211c4a515f724b52575850f
Marvin <1254401125@mil>
2019-10-15 13:53:50 -0500

    Testing Vertical tabs

--- index.js
+++ index.js
@@ -5,53 +5,53 @@ var yearArray = [];
 var ctx = null;
 var chart = null;
 
-// window.onload = function() {
+window.onload = function() {
 
-//     $.get("testNames.txt", function(data){
-//         var rows = data.split("\n");
-//         console.log("rows are: ");
-//         console.log(rows);
-//         for(row in rows){
-//             var columnsInRow = rows[row].split(",");
-//             var name = columnsInRow[0];
-//             var gender = columnsInRow[1];
-//             var count = columnsInRow[2];
-//             var year = columnsInRow[3];
+    $.get("testNames.txt", function(data){
+        var rows = data.split("\n");
+        console.log("rows are: ");
+        console.log(rows);
+        for(row in rows){
+            var columnsInRow = rows[row].split(",");
+            var name = columnsInRow[0];
+            var gender = columnsInRow[1];
+            var count = columnsInRow[2];
+            var year = columnsInRow[3];
 
-//             nameArray.push(name);
-//             genderArray.push(gender);
-//             countArray.push(count);
-//             yearArray.push(year);
-//         }
+            nameArray.push(name);
+            genderArray.push(gender);
+            countArray.push(count);
+            yearArray.push(year);
+        }
 
-//         console.log(data.toString());
+        console.log(data.toString());
 
-//         console.log("Names are");
-//         console.log(nameArray);
-//         console.log("Counts are");
-//         console.log(countArray);
+        console.log("Names are");
+        console.log(nameArray);
+        console.log("Counts are");
+        console.log(countArray);
     
-//         var ctx = document.getElementById('myChart').getContext('2d');
-//         var chart = new Chart(ctx, {
-//             // The type of chart we want to create
-//             type: 'bar',
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'bar',
     
-//             // The data for our dataset
-//             data: {
-//                 labels: nameArray,
-//                 datasets: [{
-//                     label: 'My First dataset',
-//                     backgroundColor: 'rgb(255, 99, 132)',
-//                     borderColor: 'rgb(255, 99, 132)',
-//                     data: countArray
-//                 }]
-//             },
+            // The data for our dataset
+            data: {
+                labels: nameArray,
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: countArray
+                }]
+            },
     
-//             // Configuration options go here
-//             options: {}
-//         });
-//     });
-// };
+            // Configuration options go here
+            options: {}
+        });
+    });
+};
 
 function makeSearch(){
     console.log("starting make search function and clearing data arrays");
