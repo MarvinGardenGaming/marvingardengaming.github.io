77b3f0fe684c1fe25c3ff3f3ef615c141863542d
Marvin <1254401125@mil>
2019-10-16 15:04:40 -0500

    Testing value on top of bar chart

--- index.js
+++ index.js
@@ -95,52 +95,41 @@ function makeSearch(){
     nameToSearch = document.getElementById("enterName").value;
     yearToSearch = document.getElementById("yearDropdown").value;
     $.get("masterList.txt", function(data){
-        var rows = data.split("\n");
-        for(row in rows){
-            var columnsInRow = rows[row].split(",");
-            var name = columnsInRow[0];
-            var gender = columnsInRow[1];
-            var count = columnsInRow[2];
-            var year = columnsInRow[3];
 
-            if(name == nameToSearch && year == yearToSearch){
-                nameArray.push(name);
-                genderArray.push(gender);
-                countArray.push(count);
-                yearArray.push(year);
-                if(gender == "F"){
-                    girlCount.push(count);
-                } else if (gender == "M"){
-                    boyCount.push(count);
-                }
-            }
-        };
-        if(girlCount.length == 0){
-            console.log("setting girl to 0");
-            girlCount = ["0"];
-        };
-
-        if(boyCount.length == 0){
-            console.log("setting boy to 0");
-            boyCount = ["0"];
-        };
-
-        
-        console.log(typeof boyCount);
-        console.log(boyCount);
-        console.log(typeof girlCount);
-        console.log(girlCount);
-
-
-        console.log(chart.config.data);
         var singleYearChecked = document.getElementById("inlineRadio1").checked;
         var yearRangeChecked = document.getElementById("inlineRadio2").checked;
-        console.log("single year checked is: ");
-        console.log(singleYearChecked);
-        console.log("year range checked is: ");
-        console.log(yearRangeChecked);
+
         if(singleYearChecked){
-            console.log("single year checked");
+            var rows = data.split("\n");
+            for(row in rows){
+                var columnsInRow = rows[row].split(",");
+                var name = columnsInRow[0];
+                var gender = columnsInRow[1];
+                var count = columnsInRow[2];
+                var year = columnsInRow[3];
+    
+                if(name == nameToSearch && year == yearToSearch){
+                    nameArray.push(name);
+                    genderArray.push(gender);
+                    countArray.push(count);
+                    yearArray.push(year);
+                    if(gender == "F"){
+                        girlCount.push(count);
+                    } else if (gender == "M"){
+                        boyCount.push(count);
+                    }
+                }
+            };
+            if(girlCount.length == 0){
+                console.log("setting girl to 0");
+                girlCount = ["0"];
+            };
+    
+            if(boyCount.length == 0){
+                console.log("setting boy to 0");
+                boyCount = ["0"];
+            };
+
             var chartData = {
                 labels: [yearToSearch],
                 datasets: [
@@ -162,7 +151,6 @@ function makeSearch(){
             chart.config.options.title.display = true;
             chart.config.options.title.text = "Showing results for the name \'" + nameToSearch + "\' in the year \'" + yearToSearch + "\'";
             chart.update();
-            console.log(chart.config.data);
         } else if (yearRangeChecked){
             console.log("year range checked");
             var chartData = {
